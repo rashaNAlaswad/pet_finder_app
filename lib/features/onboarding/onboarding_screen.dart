67dd1c752347743c1af8a62ce0029af0d40761fe
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-10-15 21:56:49 +0300

    Refactor onboarding screen layout into separate widgets for improved readability

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../core/constants/app_assets.dart';
+import '../../core/helper/navigation_extensions.dart';
 import '../../core/helper/spacing.dart';
+import '../../core/router/routes.dart';
 import '../../core/theme/app_text_styles.dart';
 import '../../core/widgets/app_elevated_button.dart';
 
@@ -15,39 +17,53 @@ class OnboardingScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            AppImages.catAndDog,
-            width: double.infinity,
-            height: 305.h,
-            fit: BoxFit.cover,
-          ),
+          _buildImage(),
           verticalSpace(30.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.h),
-            child: Column(
-              spacing: 10.h,
-              children: [
-                Text(
-                  "Find Your Best Companion With Us",
-                  style: AppTextStyles.font32whiteBold,
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  "Join & discover the best suitable pets as per your preferences in your location",
-                  style: AppTextStyles.font16greyRegular,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
+          _buildTextContent(),
           verticalSpace(40.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 45.w),
-            child: AppElevatedButton(
-              buttonText: "Get Started",
-              buttonIcon: AppIcons.pets,
-              onPressed: () {},
-            ),
+          _buildGetStartedButton(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGetStartedButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 45.w),
+      child: AppElevatedButton(
+        buttonText: "Get Started",
+        buttonIcon: AppIcons.pets,
+        onPressed: () {
+          context.pushNamed(Routes.home);
+        },
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return Image.asset(
+      AppImages.catAndDog,
+      width: double.infinity,
+      height: 305.h,
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildTextContent() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.h),
+      child: Column(
+        spacing: 10.h,
+        children: [
+          Text(
+            "Find Your Best Companion With Us",
+            style: AppTextStyles.font32whiteBold,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "Join & discover the best suitable pets as per your preferences in your location",
+            style: AppTextStyles.font16greyRegular,
+            textAlign: TextAlign.center,
           ),
         ],
       ),
