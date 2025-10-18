a7c41ec80003ef1c389bd33f1c9e531c6c934309
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-10-18 18:07:01 +0300

    Update dependencies and add mock repositories for testing
    - Added mock repositories for BreedRepository and DetailsRepository to facilitate testing with Mockito.

---
+++ test/mocks/mock_repositories.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:pet_finder_app/features/home/domain/repositories/breed_repository.dart';
+import 'package:pet_finder_app/features/details/domain/repositories/derails_repository.dart';
+
+@GenerateMocks([BreedRepository, DetailsRepository])
+void main() {}
